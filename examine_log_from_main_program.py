fe72b2cc0aa5c4af16d759562f81a887b59b78e9
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2019-01-19 16:17:59 +0100

    Show timestamp information in terminal

--- examine_log_from_main_program.py
+++ examine_log_from_main_program.py
@@ -28,21 +28,35 @@ print(fn)
 
 with open(fn, 'r') as file :
   filedata = file.read()
-filedata = filedata.replace("draw", "")
-filedata = filedata.replace("samples received: 200", "")
-
+  
 with open(fn, 'w') as file:
   file.write(filedata)
 
 a = np.loadtxt(fn, skiprows=1)
-time  = np.array(a[:, 0])/1e3
+time  = np.array(a[:, 0])
 ref_X = np.array(a[:, 1])
 ref_Y = np.array(a[:, 2])
 sig_I = np.array(a[:, 3])
+time = time - time[0]
 
-plt.plot(time - time[0], ref_X, 'x-r')
-plt.plot(time - time[0], ref_Y, 'x-y')
-plt.plot(time - time[0], sig_I, 'x-b')
+time_diff = np.diff(time)
+print("time_diff:")
+print("  median = %i usec" % np.median(time_diff))
+print("  mean   = %i usec" % np.mean(time_diff))
+print("  min = %i usec" % np.min(time_diff))
+print("  max = %i usec" % np.max(time_diff))
+
+time_ = time[:-1]
+time_gaps = time_[time_diff > 500]
+time_gap_durations = time_diff[time_diff > 500]
+print("number of gaps > 500 usec: %i" % len(time_gaps))
+for i in range(len(time_gaps)):
+    print("  gap %i @ t = %.3f msec for %.3f msec" %
+          (i+1, time_gaps[i]/1e3, time_gap_durations[i]/1e3))
+
+plt.plot(time/1e3, ref_X, 'x-r')
+plt.plot(time/1e3, ref_Y, 'x-y')
+plt.plot(time/1e3, sig_I, 'x-b')
 plt.grid()
 plt.xlabel("time (ms)")
 plt.ylabel("voltage (V)")
