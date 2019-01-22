d78824086200566201dfd6b677c6ae71ba44054d
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2019-01-22 19:39:52 +0100

    Update test_filters.py

--- test_filters.py
+++ test_filters.py
@@ -7,74 +7,96 @@ Created on Thu Dec  6 21:22:33 2018
 
 import numpy as np
 from scipy.signal import firwin, iirfilter, filtfilt
+from collections import deque
+
 import matplotlib as mpl
+mpl.use("qt5agg")
 import matplotlib.pyplot as plt
+import pylab
 mpl.rcParams['lines.linewidth'] = 3.0
+marker = '-'
 
 # Original data series
 Fs = 10000;          # [Hz] sampling frequency
-total_time = .5;     # [s]
-time = np.arange(0, total_time - 1/Fs, 1/Fs) # [s]
+total_time = .4;     # [s]
+time = np.arange(0, total_time, 1/Fs) # [s]
 
 sig1_ampl    = 1
-sig1_freq_Hz = 10
+sig1_freq_Hz = 100
 
 sig2_ampl    = 1
-sig2_freq_Hz = 50
+sig2_freq_Hz = 500
 
 sig3_ampl    = 0
 sig3_freq_Hz = 100
 
-sig = (sig1_ampl * np.sin(2*np.pi*time * sig1_freq_Hz) + 
-       sig2_ampl * np.sin(2*np.pi*time * sig2_freq_Hz) + 
-       sig3_ampl * np.sin(2*np.pi*time * sig3_freq_Hz))
-np.random.seed(0)
+sig1 = sig1_ampl * np.sin(2*np.pi*time * sig1_freq_Hz)
+sig2 = sig2_ampl * np.sin(2*np.pi*time * sig2_freq_Hz)
+sig3 = sig3_ampl * np.sin(2*np.pi*time * sig3_freq_Hz)
+sig = sig1 + sig2 + sig3
+
+#np.random.seed(0)
 #sig = sig + np.random.randn(len(sig))
 
-plt.plot(time, sig, ('0.8'))
+plt.plot(time, sig1, marker, color='k')
+plt.plot(time, sig2, marker, color='k')
+plt.plot(time, sig, marker, color=('0.8'))
 
 # -----------------------------------------
 #   FIR filters
 # -----------------------------------------
 
-BLOCK_SIZE = 500
-NTAPS = 201;
+BUFFER_SIZE = 500  # [samples]
+N_taps = 491       # [samples] Use an odd number!
 
-num_valid = BLOCK_SIZE - NTAPS + 1
-offset_valid = int((NTAPS - 1)/2)
 
-b_LP = firwin(NTAPS, [0.0001, 0.05], width=0.05, fs=Fs, pass_zero=False)
-b_HP = firwin(NTAPS, 0.05          , width=0.05, fs=Fs, pass_zero=False)
-b_BP = firwin(NTAPS, [0.019, 0.021], width=0.01, fs=Fs, pass_zero=False)
-b_BG = firwin(NTAPS, ([0.0001, 0.015, 0.025, .5-1e-4]), width=0.005, fs=Fs, pass_zero=False)
+BUFFER_TIME = BUFFER_SIZE / Fs
+print('buffer_time = %.3f s' % BUFFER_TIME)
+print('min freq.   = %.3f Hz' % (1/BUFFER_TIME)) # TO DO: figure out correctly
 
-iter_no = 0;
-while True:
-    idx_start = iter_no * num_valid
-    idx_end   = idx_start + BLOCK_SIZE
-    #idx_start = iter_no * BLOCK_SIZE
-    #idx_end   = idx_start + BLOCK_SIZE
-    
-    if idx_end > len(time):
-        break
-    color = 'r' if (iter_no % 2 == 0) else 'g'
-    iter_no += 1
-    
-    sig_LP = np.convolve(sig[idx_start:idx_end], b_LP, mode='valid')
-    sig_HP = np.convolve(sig[idx_start:idx_end], b_HP, mode='valid')
-    sig_BP = np.convolve(sig[idx_start:idx_end], b_BP, mode='valid')
-    sig_BG = np.convolve(sig[idx_start:idx_end], b_BG, mode='valid')
-    
-    time_sel_valid = time[idx_start + offset_valid:
-                          idx_start + offset_valid + num_valid]
-    
-    plt.plot(time_sel_valid, sig_HP, 'y')
-    plt.plot(time_sel_valid, sig_LP, color)
-    #plt.plot(time_sel_valid, sig_LP + sig_HP, 'k')
-    #plt.plot(time_sel_valid, sig_BP, 'r')
-    #plt.plot(time_sel_valid, sig_BG, 'm')
-    #plt.plot(time_sel_valid, sig_BP + sig_BG, 'k')
+N_shift_buffers = 2
+maxlen = N_shift_buffers * BUFFER_SIZE
+hist_time = deque(maxlen=maxlen)
+hist_sig  = deque(maxlen=maxlen)
 
+N_valid = BUFFER_SIZE - N_taps + 1 # [samples]
+offset_valid = int((N_taps - 1)/2)
+
+f_LP = 110
+width_LP = 10
+b_LP = firwin(N_taps, f_LP, width=width_LP, fs=Fs)
+b_HP = firwin(N_taps, 0.05          , width=0.05, fs=Fs, pass_zero=False)
+b_BP = firwin(N_taps, [0.019, 0.021], width=0.01, fs=Fs, pass_zero=False)
+b_BG = firwin(N_taps, ([0.0001, 0.015, 0.025, .5-1e-4]), width=0.005, fs=Fs, pass_zero=False)
+
+for i_window in range(int(len(time)/BUFFER_SIZE)):
+    # Simulate incoming buffers on the fly
+    buffer_time = time[BUFFER_SIZE * i_window:
+                       BUFFER_SIZE * (i_window + 1)]
+    buffer_sig  = sig[BUFFER_SIZE * i_window:
+                      BUFFER_SIZE * (i_window + 1)]
+        
+    hist_time.extend(buffer_time)
+    hist_sig.extend(buffer_sig)
+    
+    if i_window < N_shift_buffers - 1:
+        # Start-up
+        continue
+    
+    conv_in_selection_sig = np.array(hist_sig)[N_valid:]
+    
+    sig_LP = np.convolve(conv_in_selection_sig, b_LP, mode='valid')
+    
+    idx_valid_start = BUFFER_SIZE - offset_valid
+    idx_valid_end   = maxlen - offset_valid
+    
+    sel_valid_time = np.array(hist_time)[idx_valid_start:idx_valid_end]
+    
+    color = 'r' if (i_window % 2 == 0) else 'g'
+    #nudge = 0 if (i_window % 2 == 0) else 0.05
+    nudge = 0
+    plt.plot(sel_valid_time, sig_LP + nudge, marker + color)
+    
 """
 # -----------------------------------------
 #   IIR filters
@@ -147,8 +169,8 @@ def array_for_N(fun, N, x):
     return out
 
 #https://stackoverflow.com/questions/40483518/how-to-real-time-filter-with-scipy-and-lfilter
-ntaps=3
-b_LP = firwin(ntaps, [0.0001, 0.05], width=0.05, fs=Fs, pass_zero=False)
+N_taps=3
+b_LP = firwin(N_taps, [0.0001, 0.05], width=0.05, fs=Fs, pass_zero=False)
 filterFIR = FilterFIR(b_LP)
 sig_LP4 = array_map(filterFIR.step, sig)
 
@@ -177,7 +199,13 @@ plt.plot(time - correct_phase_lag, sig_LP4, 'y')
 """
 
 y_bars = np.array(plt.ylim()) * 1.1
-for i in range(np.mod(len(time), BLOCK_SIZE)):
-    plt.plot([i * time[BLOCK_SIZE - 1], i * time[BLOCK_SIZE - 1]], y_bars, 'k')
-plt.xlim([0, time[5 * BLOCK_SIZE]])
+for i in range(int(len(time)/BUFFER_SIZE)):
+    plt.plot([i * time[BUFFER_SIZE], i * time[BUFFER_SIZE]], y_bars, 'k')
+
+plt.title("f_LP = %i, width = %.3f" % (f_LP, width_LP))
+plt.xlim([0.125, 0.14])
+plt.ylim([-1.02, 1.02])
+
+thismanager = pylab.get_current_fig_manager()
+thismanager.window.setGeometry(500, 120, 1200, 700)
 plt.show()
