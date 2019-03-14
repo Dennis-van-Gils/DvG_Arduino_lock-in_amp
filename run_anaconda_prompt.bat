f0ce58a49cd7131987a083109966b6d645cc9517
Dennis-van-Gils <vangils.dennis@gmail.com>
2019-03-14 20:09:54 +0100

    Try both %USERPROFILE% and %PROGRAMDATA%

--- run_anaconda_prompt.bat
+++ run_anaconda_prompt.bat
@@ -1,1 +1,7 @@
- %windir%\System32\cmd.exe "/K" %USERPROFILE%\Anaconda3\Scripts\activate.bat %USERPROFILE%\Anaconda3
+@echo off
+if exist %USERPROFILE%\Anaconda3\Scripts\activate.bat (
+  %windir%\System32\cmd.exe "/K" %USERPROFILE%\Anaconda3\Scripts\activate.bat %USERPROFILE%\Anaconda3
+)
+if exist %PROGRAMDATA%\Anaconda3\Scripts\activate.bat (
+  %windir%\System32\cmd.exe "/K" %PROGRAMDATA%\Anaconda3\Scripts\activate.bat %PROGRAMDATA%\Anaconda3
+)
