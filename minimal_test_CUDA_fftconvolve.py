4e0ec6168a7c7ec72605b79ae46cea1d79aeef66
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2019-04-08 01:29:46 +0200

    CUDA fftconvolve tests

--- minimal_test_CUDA_fftconvolve.py
+++ minimal_test_CUDA_fftconvolve.py
@@ -8,8 +8,9 @@ Dennis van Gils
 
 from __future__ import division
 from numba import cuda
-import numpy
-import math
+import numpy as np
+import cupy
+import sigpy
 
 """FFTconvolve example C++
 cufftHandle _planKernel                             // you fft handle
@@ -26,60 +27,6 @@ cufftExecC2C(planKernel, PRODUCT, PRODUCT, CUFFT_INVERSE); // inverse fft on the
 MakeProductReal<<<blockSize, GridSize>>>(PRODUCT)   // extract the real part of PRODUCT
 """
 
-import numpy as np
-from numba import cuda, float32
-
-@cuda.jit
-def matmul(A, B, C):
-    # Perform square matrix multiplication of C = A * B
-    i, j = cuda.grid(2)
-    if i < C.shape[0] and j < C.shape[1]:
-        tmp = 0.
-        for k in range(A.shape[1]):
-            tmp += A[i, k] * B[k, j]
-        C[i, j] = tmp
-
-# Controls threads per block and shared memory usage.
-# The computation will be done on blocks of TPBxTPB elements.
-TPB = 16
-
-@cuda.jit
-def fast_matmul(A, B, C):
-    # Define an array in the shared memory
-    # The size and type of the arrays must be known at compile time
-    sA = cuda.shared.array(shape=(TPB, TPB), dtype=float32)
-    sB = cuda.shared.array(shape=(TPB, TPB), dtype=float32)
-
-    x, y = cuda.grid(2)
-
-    tx = cuda.threadIdx.x
-    ty = cuda.threadIdx.y
-    bpg = cuda.gridDim.x    # blocks per grid
-
-    if x >= C.shape[0] and y >= C.shape[1]:
-        # Quit if (x, y) is outside of valid C boundary
-        return
-
-    # Each thread computes one element in the result matrix.
-    # The dot product is chunked into dot products of TPB-long vectors.
-    tmp = 0.
-    for i in range(bpg):
-        # Preload data into shared memory
-        sA[tx, ty] = A[x, ty + i * TPB]
-        sB[tx, ty] = B[tx + i * TPB, y]
-
-        # Wait until all threads finish preloading
-        cuda.syncthreads()
-
-        # Computes partial product on the shared memory
-        for j in range(TPB):
-            tmp += sA[tx, j] * sB[j, ty]
-
-        # Wait until all threads finish computing
-        cuda.syncthreads()
-
-    C[x, y] = tmp
-    
 if __name__ == "__main__":
     print(cuda.gpus) # Reads '<Managed Device 0>' if a CUDA-enabled GPU is found
     
@@ -87,8 +34,7 @@ if __name__ == "__main__":
     b = np.identity(int(1e4), dtype=np.float32)
     c = np.empty((int(1e4), int(1e4)), dtype=np.float32)
     
-    matmul(a, b, c)
+    x = cupy.array([1, 2, 3, 4, 5])
+    y = cupy.array([1, 1, 1])
+    z = sigpy.convolve(a, b)
     
-    print(np.sum(a))
-    print(np.sum(b))
-    print(np.sum(c))
