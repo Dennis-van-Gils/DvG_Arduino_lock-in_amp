1b9dea3cd374c8216db08adc4ee7d4d43b04067a
Dennis van Gils <vangils.dennis@gmail.com>
2019-04-23 19:36:25 +0200

    Improved parameters for filter design

--- FIR_filter_try_windows.py
+++ FIR_filter_try_windows.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Dennis van Gils
-13-02-2019
+23-04-2019
 """
 
 import numpy as np
@@ -15,8 +15,8 @@ mpl.rcParams['lines.linewidth'] = 2.0
 marker = '.-'
 
 # Original data series
-Fs = 10000;          # [Hz] sampling frequency
-N_taps = 40001
+Fs = 5000;          # [Hz] sampling frequency
+N_taps = 20001
 
 # Indices within window corresponding to valid filter output
 win_idx_valid_start = int((N_taps - 1)/2)
@@ -24,14 +24,14 @@ T_delay_valid_start = win_idx_valid_start/Fs
 
 # Create tap filters for upcoming convolution
 windows = ("hamming", "hanning", "blackman", "blackmanharris", ("chebwin", 50))
-firwin_cutoff = ([0.0001, 49.5, 50.5, Fs/2-0.0001])
+firwin_cutoff = ([49, 51])
 
 plt.figure(1)
 ax1 = plt.subplot(2, 1, 1)
 ax2 = plt.subplot(2, 1, 2)
 for i in range(len(windows)):
     window = windows[i]
-    b_BG = firwin(N_taps, firwin_cutoff, window=window, fs=Fs, pass_zero=False)
+    b_BG = firwin(N_taps, firwin_cutoff, window=window, fs=Fs, pass_zero=True)
 
     # Filter frequency response
     w, h = freqz(b_BG, worN = 2**18)
@@ -41,12 +41,12 @@ for i in range(len(windows)):
     ax2.plot(freq_table_Hz, 20 * np.log10(abs(h)), marker, label=window)
 
 plt.sca(ax1)
-plt.title('Fs = %.0f Hz, N_taps = %i, T_delay = %.1f s\ncutoff = %s' %
+plt.title('Fs = %.0f Hz, N_taps = %i, T_settle = %.1f s\ncutoffs = %s' %
           (Fs,
            N_taps,
            T_delay_valid_start,
            [round(x, 1) for x in firwin_cutoff]))
-plt.ylabel('amplitude (dB)')
+plt.ylabel('ampl. attn. (dB)')
 plt.xlabel('frequency (Hz)')
 plt.xlim([48, 52])
 plt.ylim([-110, 1])
@@ -54,7 +54,7 @@ plt.grid()
 plt.legend()
 
 plt.sca(ax2)
-plt.ylabel('amplitude (dB)')
+plt.ylabel('ampl. attn. (dB)')
 plt.xlabel('frequency (Hz)')
 plt.xlim([50.5, 54])
 plt.ylim([-.4, .1])
