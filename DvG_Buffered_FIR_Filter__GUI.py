63a0a322a83b05e034c960f17873dcbc78f871b1
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2019-06-01 02:25:25 +0200

    Improved 'FIR filter design'-table UI

--- DvG_Buffered_FIR_Filter__GUI.py
+++ DvG_Buffered_FIR_Filter__GUI.py
@@ -5,7 +5,7 @@
 __author__      = "Dennis van Gils"
 __authoremail__ = "vangils.dennis@gmail.com"
 __url__         = "https://github.com/Dennis-van-Gils"
-__date__        = "31-05-2019"
+__date__        = "01-06-2019"
 __version__     = "1.0.0"
 
 import numpy as np
@@ -188,6 +188,7 @@ class Filter_design_GUI(QtCore.QObject):
         # cutoff list cannot be empty. Force at least one value in the list
         if np.size(cutoff) == 0:
             cutoff = np.append(cutoff, 1.0)
+            # And refrain from displaying a QMessageBox
         
         for firf in self.firfs:
             firf.compute_firwin(cutoff=cutoff, pass_zero=pass_zero)
@@ -217,10 +218,10 @@ class Filter_design_GUI(QtCore.QObject):
             
             msg = QtGui.QMessageBox()
             msg.setIcon(QtGui.QMessageBox.Information)
-        
-            msg.setText("Filter design")
-            msg.setInformativeText("The list can not be empty.")
-            msg.setWindowTitle("Filter design")            
+            msg.setWindowTitle("FIR filter design")
+            msg.setText("The list cannot be empty when input coupling is set "
+                        "to DC.<br>Putting 1.0 Hz back into the list.<br><br>"
+                        "You could set the input coupling to AC first.")
             msg.setStandardButtons(QtGui.QMessageBox.Ok)
             msg.exec_()
         
@@ -230,13 +231,29 @@ class Filter_design_GUI(QtCore.QObject):
     def qtbl_bandstop_KeyPressEvent(self, event):
         # Handle special events
         if event.key() == QtCore.Qt.Key_Delete:
-            self.qtbl_bandstop_cellChanged_lock = True
+            msg = QtGui.QMessageBox()
+            msg.setIcon(QtGui.QMessageBox.Information)
+            msg.setWindowTitle("FIR filter design")
+            msg.setText("Are you sure you want to remove the<br>"
+                        "following frequencies from the list?")
+            list_freqs = np.array([])
             for item in self.qtbl_bandstop.selectedItems():
-                item.setText('')
-            self.qtbl_bandstop_cellChanged_lock = False
-
-            self.construct_cutoff_list()
-            self.update_filter_design()
+                if item.text() != '':
+                    list_freqs = np.append(list_freqs, item.text())
+            str_freqs = ", ".join(map(str, list_freqs))
+            msg.setInformativeText("%s [Hz]" % str_freqs)
+            msg.setStandardButtons(QtGui.QMessageBox.Yes |
+                                   QtGui.QMessageBox.No)
+            answer = msg.exec_()
+            
+            if answer == QtGui.QMessageBox.Yes:
+                self.qtbl_bandstop_cellChanged_lock = True
+                for item in self.qtbl_bandstop.selectedItems():
+                    item.setText('')
+                self.qtbl_bandstop_cellChanged_lock = False
+    
+                self.construct_cutoff_list()
+                self.update_filter_design()
         
         # Regular event handling for QTableWidgets
         return QtGui.QTableWidget.keyPressEvent(self.qtbl_bandstop, event)
