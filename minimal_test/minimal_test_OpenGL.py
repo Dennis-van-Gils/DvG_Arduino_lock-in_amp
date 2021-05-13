17556d0d817b9325ed0912140a7ebf651a2297a3
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2021-05-13 11:30:41 +0200

    Rename `DvG_monkeypatch_pyqtgraph` to `dvg_monkeypatch_pyqtgraph`. Code style `Black`.

--- minimal_test/minimal_test_OpenGL.py
+++ minimal_test/minimal_test_OpenGL.py
@@ -18,7 +18,6 @@ import os
 import sys
 
 from PyQt5 import QtCore
-from PyQt5.QtCore import QSize
 from PyQt5 import QtWidgets as QtWid
 
 import OpenGL.GL as gl
@@ -30,68 +29,76 @@ if USE_OPENGL:
     pg.setConfigOptions(useOpenGL=True)
     pg.setConfigOptions(enableExperimental=True)
     pg.setConfigOptions(antialias=False)
-    
+
     # Monkey patch error in pyqtgraph
-    import DvG_monkeypatch_pyqtgraph as pgmp
+    import dvg_monkeypatch_pyqtgraph as pgmp
+
     pg.PlotCurveItem.paintGL = pgmp.PlotCurveItem_paintGL
 else:
     pg.setConfigOptions(useOpenGL=False)
     pg.setConfigOptions(enableExperimental=True)
     pg.setConfigOptions(antialias=False)
 
+
 @QtCore.pyqtSlot()
 def getOpenglInfo():
     """Seems only possible to be called after window.show(). I guess OpenGL has
     not initialized yet properly before window.show()
     """
-    info = ("  Vendor  : %s\n"
-            "  Renderer: %s\n"
-            "  OpenGL  : %s\n"
-            "  Shader  : %s\n" % (
-                    gl.glGetString(gl.GL_VENDOR).decode(),
-                    gl.glGetString(gl.GL_RENDERER).decode(),
-                    gl.glGetString(gl.GL_VERSION).decode(),
-                    gl.glGetString(gl.GL_SHADING_LANGUAGE_VERSION).decode()))
+    info = (
+        "  Vendor  : %s\n"
+        "  Renderer: %s\n"
+        "  OpenGL  : %s\n"
+        "  Shader  : %s\n"
+        % (
+            gl.glGetString(gl.GL_VENDOR).decode(),
+            gl.glGetString(gl.GL_RENDERER).decode(),
+            gl.glGetString(gl.GL_VERSION).decode(),
+            gl.glGetString(gl.GL_SHADING_LANGUAGE_VERSION).decode(),
+        )
+    )
     return info
+
 
 class Window(QtWid.QWidget):
     def __init__(self):
         super(Window, self).__init__()
-        #self.setGeometry(800, 400, 800, 600)
+        # self.setGeometry(800, 400, 800, 600)
         self.setWindowTitle("PyOpenGL demo in pyqtgraph")
-        
+
         num_points = int(5e3)
         x = np.array(np.arange(num_points), dtype=float)
         y = np.random.rand(num_points)
-        
+
         # NOTE: the following two lines seem only to work in Windows with OpenGL.
         # When running under Linux, the GraphicsWindow is placed outside of the
         # main app window into a separate window
-        #self.gw = pg.GraphicsWindow()
-        #self.pi = self.gw.addPlot()
-        
+        # self.gw = pg.GraphicsWindow()
+        # self.pi = self.gw.addPlot()
+
         # This works under both Windows and Linux
         self.pi = pg.PlotWidget()
-        
-        self.pi.plot(x=x, y=y, pen=pg.mkPen(color=[255, 30 , 180], width=3))
+
+        self.pi.plot(x=x, y=y, pen=pg.mkPen(color=[255, 30, 180], width=3))
         self.pi.setAutoVisible(x=True, y=True)
-        self.pi.enableAutoRange('x', False)
-        self.pi.enableAutoRange('y', False)
+        self.pi.enableAutoRange("x", False)
+        self.pi.enableAutoRange("y", False)
         self.pi.setClipToView(True)
-        self.pi.setTitle('PyOpenGL support = %s' % USE_OPENGL)
-        
+        self.pi.setTitle("PyOpenGL support = %s" % USE_OPENGL)
+
         self.qpbt_info = QtWid.QPushButton("OpenGL info")
         self.qpbt_info.clicked.connect(lambda: print(getOpenglInfo()))
-        
+
         main_layout = QtWid.QHBoxLayout()
-        #main_layout.addWidget(self.gw)
+        # main_layout.addWidget(self.gw)
         main_layout.addWidget(self.pi)
         main_layout.addWidget(self.qpbt_info)
         self.setLayout(main_layout)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print("PID: %s\n" % os.getpid())
-    
+
     app = 0
     app = QtWid.QApplication(sys.argv)
     window = Window()
