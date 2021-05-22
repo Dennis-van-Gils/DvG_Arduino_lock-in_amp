3ea33521f05a5d0de1408d02f9743136951f17c6
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2021-05-22 23:17:43 +0200

    In progress: `dvg_ringbuffer_fir_filter`

--- dvg_ringbuffer_fir_filter.py
+++ dvg_ringbuffer_fir_filter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+TODO: rename buffer_size into block_size, and buffer into block
 
 FIR: finite impulse response
 Zero-phase distortion filter, aka linear filter
@@ -25,8 +26,20 @@ from dvg_fftw_convolver import FFTW_Convolver_Valid1D
 class Ringbuffer_FIR_Filter:
     """Ringbuffer_FIR_Filter description...
 
-    Acts upon a `DvG_RingBuffer` class instance, which we call incorrectly a
-    'deque` in this module.
+    Use-case:
+    Real-time data acquisition and processing, hence the use of a ringbuffer
+    where timeseries data are continuously being appended to in chunks of size
+    `buffer_size` (TODO: should be renamed block_size).
+
+    The FIR filter output is (obviously) delayed with respect to the incoming
+    timeseries data, namely by `T_settle_filter` seconds. Attribute
+    `valid_slice` will contain the slice to be taken from the incoming
+    ringbuffer corresponding to the matching time... bla...
+
+    The class name implies that the FIR filter takes in a `DvG_RingBuffer` class
+    instance containing timeseries data. It does not mean that the FIR filter
+    output is a ringbuffer. The ringbuffer is referenced as a 'deque` throughout
+    this module.
 
     Args:
         buffer_size: int
@@ -84,13 +97,36 @@ class Ringbuffer_FIR_Filter:
             Default: False
 
     Attributes:
-        window_description:
-        T_settle_filter:
-        T_settle_deque:
-        was_deque_settled:
-        has_deque_settled:
+        config: Config()
+        display_name: str
         freqz: FreqResponse()
+        was_deque_settled: bool
+        has_deque_settled: bool
     """
+
+    class Config:
+        """In progress
+        """
+
+        # pylint: disable=too-many-instance-attributes, too-few-public-methods
+        def __init__(self):
+            self.Fs = None
+            self.buffer_size = None
+            self.N_buffers_in_deque = None
+            self.cutoff = None
+            self.window = None
+            self.pass_zero = None
+            self.use_CUDA = None
+
+            # Derived parameters
+            self.window_description = None
+            self.N_deque = None
+            self.N_taps = None
+            self.T_span_taps = None
+            self.T_span_buffer = None
+            self.valid_deque_slice = None
+            self.T_settle_filter = None
+            self.T_settle_deque = None
 
     class FreqResponse:
         """Container for the calculated frequency response of the filter based
@@ -101,110 +137,83 @@ class Ringbuffer_FIR_Filter:
 
         # pylint: disable=too-many-instance-attributes, too-few-public-methods
         def __init__(self):
-            self.full_freq_Hz = np.nan  # was `full_resp_freq_Hz`
-            self.full_ampl_dB = np.nan  # was `full_resp_ampl_dB`
-            self.full_phase_rad = np.nan  # was `full_resp_phase_rad`
-            self.freq_Hz = np.nan  # was `resp_freq_Hz`
-            self.ampl_dB = np.nan  # was `resp_ampl_dB`
-            self.phase_rad = np.nan  # was `resp_phase_rad`
-            self.freq_Hz__ROI_start = np.nan  # was `resp_freq_Hz__ROI_start`
-            self.freq_Hz__ROI_end = np.nan  # was `resp_freq_Hz__ROI_end`
+            self.full_freq_Hz = None  # was `full_resp_freq_Hz`
+            self.full_ampl_dB = None  # was `full_resp_ampl_dB`
+            self.full_phase_rad = None  # was `full_resp_phase_rad`
+            self.freq_Hz = None  # was `resp_freq_Hz`
+            self.ampl_dB = None  # was `resp_ampl_dB`
+            self.phase_rad = None  # was `resp_phase_rad`
+            self.freq_Hz__ROI_start = None  # was `resp_freq_Hz__ROI_start`
+            self.freq_Hz__ROI_end = None  # was `resp_freq_Hz__ROI_end`
 
-    class Config:
-        """In progress
-        """
-
-        def __init__(self):
-            self.buffer_size = np.nan
-            self.N_buffers_in_deque = np.nan
-            self.Fs = np.nan
-            self.cutoff = np.nan
-            self.window = "hamming"
-            self.pass_zero = True
-            self.use_CUDA = False
-
-            # Calculated based on above
-            self.window_description = ""
-            self.N_deque = np.nan
-            self.N_taps = np.nan
-            self.T_span_taps = np.nan
-            self.T_span_buffer = np.nan
-            self.valid_slice = slice(0)
-            self.T_settle_filter = np.nan
-            self.T_settle_deque = np.nan
-
+    # pylint: disable=too-many-arguments
     def __init__(
         self,
+        Fs: float,
         buffer_size: int,
         N_buffers_in_deque: int,
-        Fs: float,
         cutoff: Union[float, List[float]],
         window: Optional[Union[str, Tuple]] = "hamming",
         pass_zero: Optional[Union[bool, str]] = True,
-        display_name: Optional[str] = "",
         use_CUDA: Optional[bool] = False,
+        display_name: Optional[str] = "",
     ):
-        self.buffer_size = buffer_size
-        self.N_buffers_in_deque = N_buffers_in_deque
-        self.Fs = Fs
-        self.cutoff = np.atleast_1d(cutoff)
-        self.window = window
-        self.pass_zero = pass_zero
-        self.display_name = (
-            display_name  # TODO: Keep as member, do not put in Config()
+        #  Passed config parameters
+        # --------------------------
+        c = self.Config()
+        c.Fs = Fs
+        c.buffer_size = buffer_size
+        c.N_buffers_in_deque = N_buffers_in_deque
+        c.cutoff = np.atleast_1d(cutoff)
+        c.window = window
+        c.pass_zero = pass_zero
+        c.use_CUDA = use_CUDA
+
+        #  Derived config parameters
+        # ---------------------------
+        c.window_description = (
+            "%s" % window
+            if isinstance(window, str)
+            else "%s" % [x for x in window]
         )
-        self.use_CUDA = use_CUDA
+        c.N_deque = c.buffer_size * c.N_buffers_in_deque  # [samples]
 
-        # Friendly window description
-        if isinstance(self.window, str):
-            self.window_description = "%s" % self.window
-        else:
-            self.window_description = "%s" % [x for x in self.window]
-
-        # Deque size
-        self.N_deque = self.buffer_size * self.N_buffers_in_deque  # [samples]
-
-        # Calculate max number of possible taps [samples]. Must be an odd number
-        # in order to create a zero-phase distortion filter, aka linear filter!
-        self.N_taps = self.buffer_size * (self.N_buffers_in_deque - 1) + 1
-        self.T_span_taps = self.N_taps / self.Fs  # [s]
-        self.T_span_buffer = self.buffer_size / self.Fs  # [s]
+        # Calculate max number of possible taps. We'll force an odd number in
+        # order to create a zero-phase distortion filter, aka linear filter.
+        c.N_taps = c.buffer_size * (c.N_buffers_in_deque - 1) + 1  # [samples]
+        c.T_span_taps = c.N_taps / c.Fs  # [s]
+        c.T_span_buffer = c.buffer_size / c.Fs  # [s]
 
         # Indices within window corresponding to valid filter output
-        # TODO: instead of storing `win_idx_valid_start` and `win_idx_valid_end`
-        # store the valid slice instead, like:
-        # valid_slice = slice(win_idx_valid_start, win_idx_valid_end)
-        self.win_idx_valid_start = int((self.N_taps - 1) / 2)
-        self.win_idx_valid_end = self.N_deque - self.win_idx_valid_start
+        idx_valid_start = int((c.N_taps - 1) / 2)
+        idx_valid_end = c.N_deque - idx_valid_start
+        c.valid_deque_slice = slice(idx_valid_start, idx_valid_end)
 
-        self.T_settle_filter = self.win_idx_valid_start / self.Fs  # [s]
-        self.T_settle_deque = self.T_settle_filter * 2  # [s]
+        # Settling times
+        c.T_settle_filter = idx_valid_start / c.Fs  # [s]
+        c.T_settle_deque = c.T_settle_filter * 2  # [s]
 
-        # TODO: Keep as members, do not put in Config()
+        #  Root level members
+        # --------------------
+        self.config = c
+        self.display_name = display_name
         self.was_deque_settled = False
         self.has_deque_settled = False
 
-        # Calculated FIR filter tap array
-        self.b = None  # Used when `use_CUDA = False`
-        self.b_cp = None  # Used when `use_CUDA = True`
+        # Container for the calculated FIR filter tap array
+        self._taps = None  # Used when `use_CUDA = False`
+        self._taps_cupy = None  # Used when `use_CUDA = True`
 
         # Container for the calculated frequency response of the filter based
         # on the output of :meth:`scipy.signal.freqz`.
         self.freqz = self.FreqResponse()
 
-        if self.use_CUDA:
+        if not c.use_CUDA:
+            # Create FFTW plans for convolution
+            self._fftw_convolver = FFTW_Convolver_Valid1D(c.N_deque, c.N_taps)
+        else:
             self.cupy = import_module("cupy")
             self.sigpy = import_module("sigpy")
-        else:
-            # Create FFTW plans for fft convolution
-            self.fftw_convolver = FFTW_Convolver_Valid1D(
-                self.N_deque, self.N_taps
-            )
-
-        # Compute the FIR filter tap array
-        # TODO: perhaps remove the method call and have the user make the call
-        # instead. Benefits: user can alter `freq_worN` and `freqz_dB_floor`.
-        self.compute_firwin()
 
     # --------------------------------------------------------------------------
     #   compute_firwin
@@ -239,48 +248,48 @@ class Ringbuffer_FIR_Filter:
             freqz_dB_floor: float
                 Decibel (noise-)floor bla... # TODO
         """
+        c = self.config  # Shorthand
+
         if cutoff is not None:
-            self.cutoff = np.atleast_1d(cutoff)
+            c.cutoff = np.atleast_1d(cutoff)
 
         # Check cutoff frequencies for illegal values and cap when necessary,
         # sort by increasing value and remove duplicates.
         #   Frequencies <= 0 Hz will be removed
         #   Frequencies >= Nyquist will be set to Nyquist - 'cutoff_grain'
         cutoff_grain = 1e-6
-        cutoff = self.cutoff
+        cutoff = c.cutoff
         cutoff = cutoff[cutoff > 0]
-        cutoff[cutoff >= self.Fs / 2] = self.Fs / 2 - cutoff_grain
-        self.cutoff = np.unique(np.sort(cutoff))
+        cutoff[cutoff >= c.Fs / 2] = c.Fs / 2 - cutoff_grain
+        c.cutoff = np.unique(np.sort(cutoff))
 
         if window is not None:
-            self.window = window
-
-            # Friendly window description
-            if isinstance(self.window, str):
-                self.window_description = "%s" % self.window
-            else:
-                self.window_description = "%s" % [x for x in self.window]
+            c.window = window
+            c.window_description = (
+                "%s" % window
+                if isinstance(window, str)
+                else "%s" % [x for x in window]
+            )
 
         if pass_zero is not None:
-            self.pass_zero = pass_zero
+            c.pass_zero = pass_zero
 
         # Calculate the FIR filter tap array
-        self.b = firwin(
-            numtaps=self.N_taps,
-            cutoff=self.cutoff,
-            window=self.window,
-            pass_zero=self.pass_zero,
-            fs=self.Fs,
+        self._taps = firwin(
+            numtaps=c.N_taps,
+            cutoff=c.cutoff,
+            window=c.window,
+            pass_zero=c.pass_zero,
+            fs=c.Fs,
         )
 
-        if self.use_CUDA:
+        if c.use_CUDA:
             # Copy FIR filter tap array from CPU to GPU memory.
             # Turning 1-D array into column vector by [:, None] for CUDA.
-            self.b_cp = self.cupy.array(self.b[:, None])
+            self._taps_cupy = self.cupy.array(self._taps[:, None])
 
         # Calculate the frequency response
         self._compute_freqz(worN=freqz_worN, dB_floor=freqz_dB_floor)
-
         # self.report()
 
     # --------------------------------------------------------------------------
@@ -299,10 +308,11 @@ class Ringbuffer_FIR_Filter:
         Note: Amplitude ratio in dB: 20 log_10(A1/A2)
               Power     ratio in dB: 10 log_10(P1/P2)
         """
-        w, h = freqz(self.b, worN=worN)
-        full_freq = w / np.pi * self.Fs / 2  # [Hz]
+        c = self.config  # Shorthand
+        w, h = freqz(self._taps, worN=worN)
+        full_freq = w / np.pi * c.Fs / 2  # [Hz]
         full_ampl = 20 * np.log10(abs(h))  # [dB]
-        full_phase = np.unwrap(np.angle(h)) / self.Fs  # [rad]
+        full_phase = np.unwrap(np.angle(h)) / c.Fs  # [rad]
 
         # ---------------------------------
         #  Select region of interest (ROI)
@@ -320,7 +330,7 @@ class Ringbuffer_FIR_Filter:
 
         if len(idx_keep) <= 1:
             freq_Hz__ROI_start = 0
-            freq_Hz__ROI_end = self.Fs / 2
+            freq_Hz__ROI_end = c.Fs / 2
         else:
             freq_Hz__ROI_start = full_freq[idx_keep[0]]
             freq_Hz__ROI_end = full_freq[idx_keep[-1]]
@@ -348,17 +358,15 @@ class Ringbuffer_FIR_Filter:
         if not (idx_keep[-1] == (len(full_ampl) - 1)):
             idx_keep = np.append(idx_keep, len(full_ampl) - 1)
 
-        # -------
-        #  Store
-        # -------
+        # ---------------
+        #  Store results
+        # ---------------
 
         self.freqz.full_freq_Hz = full_freq
         self.freqz.full_ampl_dB = full_ampl
         self.freqz.full_phase_rad = full_phase
-
         self.freqz.freq_Hz__ROI_start = freq_Hz__ROI_start
         self.freqz.freq_Hz__ROI_end = freq_Hz__ROI_end
-
         self.freqz.freq_Hz = full_freq[idx_keep]
         self.freqz.ampl_dB = ampl[idx_keep]
         self.freqz.phase_rad = full_phase[idx_keep]
@@ -373,12 +381,13 @@ class Ringbuffer_FIR_Filter:
         if the filter has settled. Any NaNs in deque_sig_in will desettle the
         filter.
         """
+        c = self.config  # Shorthand
 
         # print("%s: %i" % (self.display_name, len(deque_sig_in)))
-        if (not deque_sig_in.is_full) or np.isnan(deque_sig_in).any():
+        if not deque_sig_in.is_full or np.isnan(deque_sig_in).any():
             # Start-up. Deque still needs time to settle.
             self.has_deque_settled = False
-            valid_out = np.full(self.buffer_size, np.nan)
+            valid_out = np.full(c.buffer_size, np.nan)
         else:
             self.has_deque_settled = True
             # Select window out of the signal deque to feed into the
@@ -386,13 +395,19 @@ class Ringbuffer_FIR_Filter:
             # Returns valid filtered signal output of current window.
 
             # tick = Time.perf_counter()
-            if self.use_CUDA:
-                # Perform convolution on the GPU
 
-                # Turning 1-D array into column vector by [:, None] for CUDA
+            if not c.use_CUDA:
+                # Perform convolution on the CPU
+                valid_out = self._fftw_convolver.convolve(
+                    deque_sig_in, self._taps
+                )
+            else:
+                # Perform convolution on the GPU
                 cp_valid_out = self.sigpy.convolve(
                     self.cupy.array(np.asarray(deque_sig_in)[:, None]),
-                    self.b_cp,
+                    # Turning 1-D array into column vector by using [:, None] as
+                    # is needed for CUDA
+                    self._taps_cupy,
                     mode="valid",
                 )
 
@@ -401,15 +416,13 @@ class Ringbuffer_FIR_Filter:
 
                 # Reduce the dimension again
                 valid_out = valid_out[:, 0]
-            else:
-                # Perform convolution on the CPU
-                valid_out = self.fftw_convolver.convolve(deque_sig_in, self.b)
+
             # print("%.1f" % ((Time.perf_counter() - tick)*1000))
 
-        if self.has_deque_settled and not (self.was_deque_settled):
+        if self.has_deque_settled and not self.was_deque_settled:
             # print("%s: Deque has settled" % self.display_name)
             self.was_deque_settled = True
-        elif not (self.has_deque_settled) and self.was_deque_settled:
+        elif not self.has_deque_settled and self.was_deque_settled:
             # print("%s: Deque has reset" % self.display_name)
             self.was_deque_settled = False
 
@@ -420,22 +433,29 @@ class Ringbuffer_FIR_Filter:
     # --------------------------------------------------------------------------
 
     def report(self):
-        print("Buffered_FIR_Filter: %s" % self.display_name)
-        print("--------------------------------")
-        print("Fs                 = %.0f Hz" % self.Fs)
-        print("buffer_size        = %i samples" % self.buffer_size)
-        print("N_buffers_in_deque = %i" % self.N_buffers_in_deque)
-        print("--------------------------------")
-        print("N_taps        = %i samples" % self.N_taps)
-        print("T_span_taps   = %.3f s" % self.T_span_taps)
-        print("T_span_buffer = %.3f s" % self.T_span_buffer)
-        print("--------------------------------")
-        print("window = %s" % self.window_description)
-        print("cutoff = %s" % [round(x, 1) for x in self.cutoff])
-        print("pass_zero = %s" % self.pass_zero)
-        print("--------------------------------")
-        print("win_idx_valid_start = %i samples" % self.win_idx_valid_start)
-        print("T_settle_filter     = %.3f s" % self.T_settle_filter)
-        print("T_settle_deque      = %.3f s" % self.T_settle_deque)
-        print("--------------------------------")
+        c = self.config  # Shorthand
+
+        def f(name, value, value_format, unit=""):
+            format_str = "{:>19s}  %s  {:<s}" % value_format
+            print(format_str.format(name, value, unit))
+
+        print("\nRingbuffer_FIR_Filter `%s`" % self.display_name)
+        print("═" * 50)
+        f("Fs", c.Fs, "{:>9,.2f}", "Hz")
+        f("buffer_size", c.buffer_size, "{:>9d}", "samples")
+        f("N_buffers_in_deque", c.N_buffers_in_deque, "{:>9d}")
+        print("─" * 50)
+        f("window", c.window_description, "{:<s}")
+        f("cutoff", "%s" % [round(x, 1) for x in c.cutoff], "{:<s}", "Hz")
+        f("pass_zero", str(c.pass_zero), "{:<s}")
+        print("─" * 50)
+        f("N_deque", c.N_deque, "{:>9d}", "samples")
+        f("N_taps", c.N_taps, "{:>9d}", "samples")
+        f("T_span_taps", c.T_span_taps, "{:>9.3f}", "s")
+        f("T_span_buffer", c.T_span_buffer, "{:>9.3f}", "s")
+        print("─" * 50)
+        f("valid_deque_slice", str(c.valid_deque_slice), "{:<s}")
+        f("T_settle_filter", c.T_settle_filter, "{:>9.3f}", "s")
+        f("T_settle_deque", c.T_settle_deque, "{:>9.3f}", "s")
+        print("═" * 50)
 
