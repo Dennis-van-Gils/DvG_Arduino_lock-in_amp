51ef7e35aac2ba2d42460b15dbc32a4b41f948dc
Dennis-van-Gils <vangils.dennis@gmail.com>
2021-05-25 18:19:29 +0200

    Make scipy test optional

--- timeit/timeit_FIR_filter_fftconvolve_2021.py
+++ timeit/timeit_FIR_filter_fftconvolve_2021.py
@@ -22,24 +22,26 @@ sys.path.insert(0, parent_dir)
 
 from dvg_fftw_convolver import FFTW_Convolver_Valid1D
 
+TEST_SCIPY = False
 TEST_CUDA = False
-if TEST_CUDA:
-    from numba import cuda
-    import cupy
-    import sigpy
-
-
-def test_scipy():
-    global wave_out1
-    wave_out1 = signal.fftconvolve(wave, b_np, mode="valid")
 
 
 def test_fftw():
     global wave_out2
-    wave_out2 = fftw_convolver.convolve(wave, b_np)
+    wave_out2 = fftw_convolver.convolve(wave, taps)
+
+
+if TEST_SCIPY:
+
+    def test_scipy():
+        global wave_out1
+        wave_out1 = signal.fftconvolve(wave, taps, mode="valid")
 
 
 if TEST_CUDA:
+    from numba import cuda
+    import cupy
+    import sigpy
 
     def test_cuda():
         global wave_out3
@@ -47,7 +49,7 @@ if TEST_CUDA:
         # We also transform the 1-D array to a column vector, preferred by CUDA
         wave_cp = cupy.array(wave[:, None])
         # Perform fft convolution on the GPU
-        z_cp = sigpy.convolve(wave_cp, b_cp, mode="valid")
+        z_cp = sigpy.convolve(wave_cp, taps_cupy, mode="valid")
         # Transfer result back to CPU memory
         # And reduce dimension again
         wave_out3 = cupy.asnumpy(z_cp)[:, 0]
@@ -55,13 +57,13 @@ if TEST_CUDA:
 
 if __name__ == "__main__":
     # Generate signal
-    buffer_size = 2000  # small: 500, large: 2000
-    N_buffers_in_deque = 21  # small: 21, large: 41
-    N_taps = buffer_size * (N_buffers_in_deque - 1) + 1
-    N_deque = buffer_size * N_buffers_in_deque
+    block_size = 2000  # small: 500, large: 2000
+    N_blocks = 21  # small: 21, large: 41
+    rb_capacity = block_size * N_blocks
+    N_taps = block_size * (N_blocks - 1) + 1
 
     Fs = 5000  # [Hz]
-    t = np.arange(N_deque) * 1 / Fs  # [s]
+    t = np.arange(rb_capacity) * 1 / Fs  # [s]
     f1 = 100  # [Hz]
     f2 = 200  # [Hz]
     f_fluct = 1 + np.sin(2 * np.pi * 1 * t) / 600
@@ -69,18 +71,22 @@ if __name__ == "__main__":
     wave += np.sin(2 * np.pi * f2 * t)
 
     # Generate filter
-    b_np = signal.firwin(
-        N_taps, cutoff=[190, 210], window=("chebwin", 50), pass_zero=True, fs=Fs
+    taps = signal.firwin(
+        numtaps=N_taps,
+        cutoff=[190, 210],
+        window=("chebwin", 50),
+        pass_zero=True,
+        fs=Fs,
     )
 
     if TEST_CUDA:
-        b_cp = cupy.array(b_np[:, None])
+        taps_cupy = cupy.array(taps[:, None])
 
     # Init
-    fftw_convolver = FFTW_Convolver_Valid1D(len(wave), len(b_np))
-    wave_out1 = np.empty(N_deque - N_taps + 1)
-    wave_out2 = np.empty(N_deque - N_taps + 1)
-    wave_out3 = np.empty(N_deque - N_taps + 1)
+    fftw_convolver = FFTW_Convolver_Valid1D(len(wave), len(taps))
+    wave_out1 = np.empty(rb_capacity - N_taps + 1)
+    wave_out2 = np.empty(rb_capacity - N_taps + 1)
+    wave_out3 = np.empty(rb_capacity - N_taps + 1)
 
     # Logging
     try:
@@ -114,15 +120,16 @@ if __name__ == "__main__":
 
     report("\nN = %i, REPS = %i" % (N, REPS))
 
-    report("\nbuffer size = %i" % buffer_size)
-    report("N_buffers   = %i" % N_buffers_in_deque)
-    report("--> N_taps  = %i" % N_taps)
-    report("--> N_deque = %i" % N_deque)
+    report("\nblock size = %i" % block_size)
+    report("N_blocks   = %i" % N_blocks)
+    report("--> rb_capacity = %i" % rb_capacity)
+    report("--> N_taps      = %i" % N_taps)
 
-    result1 = np.array(timeit.repeat(test_scipy, **p)) / N * 1000
-    report("\n#1  scipy.signal.fftconvolve:")
-    for r in result1:
-        report("%20.3f ms" % r)
+    if TEST_SCIPY:
+        result1 = np.array(timeit.repeat(test_scipy, **p)) / N * 1000
+        report("\n#1  scipy.signal.fftconvolve:")
+        for r in result1:
+            report("%20.3f ms" % r)
 
     result2 = np.array(timeit.repeat(test_fftw, **p)) / N * 1000
     report("\n#2  dvg_fftw_convolver.convolve:")
@@ -135,7 +142,9 @@ if __name__ == "__main__":
         for r in result3:
             report("%20.3f ms" % r)
 
-    report("\nTimes faster #1/#2: %.2f" % (min(result1) / min(result2)))
+    if TEST_SCIPY:
+        report("\nTimes faster #1/#2: %.2f" % (min(result1) / min(result2)))
+
     if TEST_CUDA:
         report("Times faster #1/#3: %.2f" % (min(result1) / min(result3)))
 
@@ -143,9 +152,12 @@ if __name__ == "__main__":
     #   Plot for comparison
     # -----------------------------------
 
-    if True:
-        plt.plot(wave_out1, ".-")
+    if False:
         plt.plot(wave_out2, ".-r")
+
+        if TEST_SCIPY:
+            plt.plot(wave_out1, ".-")
+
         if TEST_CUDA:
             plt.plot(wave_out3, ".-k")
         plt.show()
