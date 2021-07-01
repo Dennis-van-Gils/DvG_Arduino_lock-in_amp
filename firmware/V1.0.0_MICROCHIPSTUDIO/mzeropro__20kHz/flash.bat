f57917239bc040cb577b59a33abf28fea86a8be5
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2021-07-01 21:40:01 +0200

    Improved flash procedure

--- firmware/V1.0.0_MICROCHIPSTUDIO/mzeropro__20kHz/flash.bat
+++ firmware/V1.0.0_MICROCHIPSTUDIO/mzeropro__20kHz/flash.bat
@@ -2,14 +2,17 @@
 echo;
 echo ---------------------------------------------------------
 echo   Connect to the programming port of the M0.
-echo   Before flashing make sure to erase the chip and to set
-echo   the NVMCTRL_BOOTPROT fuse to 0 bytes (EDBG tool).
 echo ---------------------------------------------------------
 echo;
 
+edbg -b -t samd21 -pv -f firmware.bin
 
-set tool=%UserProfile%\.platformio\packages\tool-openocd\bin\openocd.exe
-set scripts=%UserProfile%\.platformio\packages\tool-openocd\scripts
-set firmware=LIA_SAMD21.bin
+REM Alternative flash
+REM Before flashing make sure to erase the chip and to set
+REM the NVMCTRL_BOOTPROT fuse to 0 bytes (EDBG tool).
 
-%tool% -d2 -s %scripts% -f interface/cmsis-dap.cfg -c "set CHIPNAME at91samd21g18" -f target/at91samdXX.cfg -c "program {%firmware%} 0x0000 verify reset; shutdown;"
+REM set tool=%UserProfile%\.platformio\packages\tool-openocd\bin\openocd.exe
+REM set scripts=%UserProfile%\.platformio\packages\tool-openocd\scripts
+REM set firmware=firmware.bin
+
+REM %tool% -d2 -s %scripts% -f interface/cmsis-dap.cfg -c "set CHIPNAME at91samd21g18" -f target/at91samdXX.cfg -c "program {%firmware%} 0x0000 verify reset; shutdown;"
--- firmware/v1.0.0_VSCODE/mzeropro__10kHz/flash.bat
+++ firmware/v1.0.0_VSCODE/mzeropro__10kHz/flash.bat
@@ -1,16 +1,21 @@
 @echo off
 echo;
 echo ---------------------------------------------------------
-echo   Connect to the native port of the M0.
-echo   Port should be set to the port visible when running the
-echo   M0 in boot mode.
+echo   Connect to the programming port of the M0.
 echo ---------------------------------------------------------
 echo;
 
+edbg -b -t samd41 -pv -o 0x4000 -f firmware.bin
 
-set port=COM13
-set tool=%UserProfile%\.platformio\packages\tool-avrdude\avrdude.exe
-set conf=%UserProfile%\.platformio\packages\tool-avrdude\avrdude.conf
-set firmware=firmware.hex
 
-%tool% -v -p atmega2560 -C %conf% -c stk500v2 -P %port% -b 57600 -u -U flash:w:%firmware%:i
+REM Alternative flash
+REM Connect to the native port of the M0.
+REM Port should be set to the port visible when running the
+REM M0 in boot mode.
+
+REM set port=COM13
+REM set tool=%UserProfile%\.platformio\packages\tool-avrdude\avrdude.exe
+REM set conf=%UserProfile%\.platformio\packages\tool-avrdude\avrdude.conf
+REM set firmware=firmware.hex
+
+REM %tool% -v -p atmega2560 -C %conf% -c stk500v2 -P %port% -b 57600 -u -U flash:w:%firmware%:i
