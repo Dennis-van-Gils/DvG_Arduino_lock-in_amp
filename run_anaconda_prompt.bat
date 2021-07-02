e59d0c4a8a48a23b367006d147c0f8dbbf86f846
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2021-07-02 22:57:31 +0200

    Delete run_anaconda_prompt.bat

--- run_anaconda_prompt.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if exist %USERPROFILE%\Anaconda3\Scripts\activate.bat (
-  %windir%\System32\cmd.exe "/K" %USERPROFILE%\Anaconda3\Scripts\activate.bat %USERPROFILE%\Anaconda3
-)
-if exist %PROGRAMDATA%\Anaconda3\Scripts\activate.bat (
-  %windir%\System32\cmd.exe "/K" %PROGRAMDATA%\Anaconda3\Scripts\activate.bat %PROGRAMDATA%\Anaconda3
-)
