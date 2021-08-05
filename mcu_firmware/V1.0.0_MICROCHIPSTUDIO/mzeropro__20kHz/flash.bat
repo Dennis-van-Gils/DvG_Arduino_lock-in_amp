18e9f2cf99e53493b384bb289472591538544aae
Dennis-van-Gils <vangils.dennis@gmail.com>
2021-08-05 18:02:56 +0200

    Respect the bootloader (16384 kB, 0x4000)

--- mcu_firmware/V1.0.0_MICROCHIPSTUDIO/mzeropro__20kHz/flash.bat
+++ mcu_firmware/V1.0.0_MICROCHIPSTUDIO/mzeropro__20kHz/flash.bat
@@ -5,7 +5,8 @@ echo   Connect to the programming port of the M0.
 echo ---------------------------------------------------------
 echo;
 
-edbg -b -t samd21 -pv -f firmware.bin
+edbg -b -t samd21 -pv -o 0x4000 -f firmware.bin
+
 
 REM Alternative flash
 REM Before flashing make sure to erase the chip and to set
@@ -15,4 +16,4 @@ REM set tool=%UserProfile%\.platformio\packages\tool-openocd\bin\openocd.exe
 REM set scripts=%UserProfile%\.platformio\packages\tool-openocd\scripts
 REM set firmware=firmware.bin
 
-REM %tool% -d2 -s %scripts% -f interface/cmsis-dap.cfg -c "set CHIPNAME at91samd21g18" -f target/at91samdXX.cfg -c "program {%firmware%} 0x0000 verify reset; shutdown;"
+REM %tool% -d2 -s %scripts% -f interface/cmsis-dap.cfg -c "set CHIPNAME at91samd21g18" -f target/at91samdXX.cfg -c "program {%firmware%} 0x4000 verify reset; shutdown;"
