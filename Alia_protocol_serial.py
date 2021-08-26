Corrected units of `ref_X/Y` from [V] to [non-dim], removed wrong `True RMS`

Alia_protocol_serial.py
@@ -6,7 +6,7 @@ connection.
 __author__ = "Dennis van Gils"
 __authoremail__ = "vangils.dennis@gmail.com"
 __url__ = "https://github.com/Dennis-van-Gils/DvG_Arduino_lock-in_amp"
-__date__ = "10-08-2021"
+__date__ = "26-08-2021"
 __version__ = "2.0.0"
 # pylint: disable=bare-except, broad-except, pointless-string-statement, invalid-name
 
@@ -97,9 +97,10 @@ class Alia(Arduino_protocol_serial.Arduino):
         # `LUT_X` and `LUT_Y` will contain a single period each of the
         # reference signals, where `Y` is phase-shifted by 90 degrees, i.e. the
         # quadrant. Both will get (re)computed based on the current reference
-        # signal parameters.
-        LUT_X = np.array([], dtype=float, order="C")  # [V]
-        LUT_Y = np.array([], dtype=float, order="C")  # [V]
+        # signal parameters. Both `LUT_X` and `LUT_Y` are non-dimensional and
+        # can directly be used for heterodyne mixing.
+        LUT_X = np.array([], dtype=float, order="C")  # [non-dim]
+        LUT_Y = np.array([], dtype=float, order="C")  # [non-dim]
 
         # Reference signal parameters
         ref_waveform       = Waveform.Unknown  # Waveform enum
@@ -693,7 +694,7 @@ class Alia(Arduino_protocol_serial.Arduino):
             #       Legacy firmware
             # ---------------------------
 
-            c.ref_RMS_factor = np.sqrt(2)  # Fixed to `Cosine`
+            pass
 
         else:
 
@@ -702,19 +703,18 @@ class Alia(Arduino_protocol_serial.Arduino):
             # ---------------------------
 
             # Reconstruct `LUT_X` and `LUT_Y` in advance
+            # Interval [-1, 1]
             if c.ref_waveform == Waveform.Cosine:
                 c.ref_RMS_factor = np.sqrt(2)
-
-                phis = np.linspace(0, 2 * np.pi, num=c.N_LUT, endpoint=False)
-                LUT_X = 0.5 * (1 + np.cos(phis))
-                LUT_Y = 0.5 * (1 + np.sin(phis))
+                idxs = np.arange(0, c.N_LUT)
+                LUT_X = np.cos(2 * np.pi * idxs / c.N_LUT)
+                LUT_Y = np.sin(2 * np.pi * idxs / c.N_LUT)
 
             elif c.ref_waveform == Waveform.Square:
                 c.ref_RMS_factor = 1
-
                 idxs = np.arange(0, c.N_LUT)
                 LUT_X = np.sign(np.cos(2 * np.pi * idxs / c.N_LUT))
-                LUT_X[LUT_X < 0] = 0
+                LUT_X[LUT_X == 0] = -1
 
                 if c.N_LUT % 4 > 0:
                     # Not an integer multiple of 4
@@ -727,15 +727,16 @@ class Alia(Arduino_protocol_serial.Arduino):
                 else:
                     # Perfect quadrant exists
                     LUT_Y = np.sign(np.sin(2 * np.pi * idxs / c.N_LUT))
-                    LUT_Y[LUT_Y < 0] = 0
+                    LUT_Y[LUT_Y == 0] = -1
 
             elif c.ref_waveform == Waveform.Triangle:
                 c.ref_RMS_factor = np.sqrt(3)
-
                 idxs = np.arange(0, c.N_LUT)
-                LUT_X = 2 * np.abs(idxs / c.N_LUT - 0.5)
-                LUT_Y = 2 * np.abs(
-                    ((idxs - c.N_LUT / 4) % c.N_LUT) / c.N_LUT - 0.5
+                LUT_X = 2 * np.abs(2 * idxs / c.N_LUT - 1) - 1
+                LUT_Y = (
+                    2
+                    * np.abs(2 * ((idxs - c.N_LUT / 4) % c.N_LUT) / c.N_LUT - 1)
+                    - 1
                 )
 
             elif c.ref_waveform == Waveform.Unknown:
@@ -744,11 +745,9 @@ class Alia(Arduino_protocol_serial.Arduino):
                 LUT_X = np.full(np.nan, c.N_LUT)
                 LUT_Y = np.full(np.nan, c.N_LUT)
 
-            # Transform [0, 1] to [V]
-            LUT_X = (c.ref_V_offset - c.ref_V_ampl) + 2 * c.ref_V_ampl * LUT_X
-            LUT_Y = (c.ref_V_offset - c.ref_V_ampl) + 2 * c.ref_V_ampl * LUT_Y
-            LUT_X.clip(0, c.A_REF, out=LUT_X)
-            LUT_Y.clip(0, c.A_REF, out=LUT_Y)
+            # Scale the interval [-1, 1] with the RMS factor
+            LUT_X *= c.ref_RMS_factor
+            LUT_Y *= c.ref_RMS_factor
 
             c.LUT_X = np.asarray(LUT_X, dtype=float, order="C")
             c.LUT_Y = np.asarray(LUT_Y, dtype=float, order="C")
@@ -971,8 +970,8 @@ class Alia(Arduino_protocol_serial.Arduino):
                 success: bool
                 counter: int | numpy.nan
                 time   : numpy.ndarray, units [us]
-                ref_X  : numpy.ndarray, units [V]
-                ref_Y  : numpy.ndarray, units [V]
+                ref_X  : numpy.ndarray, units [non-dim]
+                ref_Y  : numpy.ndarray, units [non-dim]
                 sig_I  : numpy.ndarray, units [V]
             )
         """
@@ -1042,15 +1041,21 @@ class Alia(Arduino_protocol_serial.Arduino):
                 phi = np.unwrap(phi + phase_offset_deg / 180 * np.pi)
 
             # Construct `ref_X` and `ref_Y`
-            ref_X = np.cos(phi)
-            ref_Y = np.sin(phi)
 
+            """
+            if c.ref_waveform == Waveform.Cosine:
             # OVERRIDE: Only `Cosine` allowed, because `Square` and `Triangle`
             # can not be garantueed deterministic on both Arduino and Python
             # side due to rounding differences and the problem of computing the
             # correct 90 degrees quadrant `ref_Y`.
             """
+            c.ref_RMS_factor = np.sqrt(2)
+            ref_X = np.cos(phi)
+            ref_Y = np.sin(phi)
+
+            """
             if c.ref_waveform == Waveform.Square:
+                c.ref_RMS_factor = 1
                 ref_X.fill(-1)
                 ref_X[ref_X_phase < (c.N_LUT / 4.0)] = 1.0
                 ref_X[ref_X_phase >= (c.N_LUT / 4.0 * 3.0)] = 1.0
@@ -1059,15 +1064,14 @@ class Alia(Arduino_protocol_serial.Arduino):
                 ref_Y[ref_X_phase == (c.N_LUT / 2.0)] = 0.0
 
             if c.ref_waveform == Waveform.Triangle:
+                c.ref_RMS_factor = np.sqrt(3)
                 ref_X = np.arcsin(ref_X) / np.pi * 2
                 ref_Y = np.arcsin(ref_Y) / np.pi * 2
             """
 
-            np.multiply(ref_X, c.ref_V_ampl, out=ref_X)
-            np.multiply(ref_Y, c.ref_V_ampl, out=ref_Y)
-
-            ref_X = (c.ref_V_offset + ref_X).clip(0, c.A_REF)
-            ref_Y = (c.ref_V_offset + ref_Y).clip(0, c.A_REF)
+            # Scale the interval [-1, 1] with the RMS factor
+            np.multiply(ref_X, c.ref_RMS_factor, out=ref_X)
+            np.multiply(ref_Y, c.ref_RMS_factor, out=ref_Y)
 
             # Transform `sig_I` from [bits] to [V]
             sig_I = np.multiply(sig_I, c.ADC_BITS_TO_V, out=sig_I)

Alia_qdev.py
@@ -7,7 +7,7 @@ specific firmware to turn it into a lock-in amplifier.
 __author__ = "Dennis van Gils"
 __authoremail__ = "vangils.dennis@gmail.com"
 __url__ = "https://github.com/Dennis-van-Gils/DvG_dev_Arduino"
-__date__ = "10-08-2021"
+__date__ = "26-08-2021"
 __version__ = "2.0.0"
 # pylint: disable=invalid-name, missing-function-docstring
 
@@ -122,7 +122,6 @@ class Alia_qdev(QDeviceIO):
             self.X_avg      = np.nan  # [V_RMS]
             self.Y_avg      = np.nan  # [V_RMS]
             self.R_avg      = np.nan  # [V_RMS]
-            self.R_avg_trms = np.nan  # [V_TRMS], i.e. 'True RMS'
             self.T_avg      = np.nan  # [deg]
             # fmt: on
 

DvG_Arduino_lockin_amp.py
@@ -5,7 +5,7 @@
 __author__ = "Dennis van Gils"
 __authoremail__ = "vangils.dennis@gmail.com"
 __url__ = "https://github.com/Dennis-van-Gils/DvG_Arduino_lock-in_amp"
-__date__ = "06-08-2021"
+__date__ = "26-08-2021"
 __version__ = "2.0.0"
 # pylint: disable=invalid-name
 
@@ -172,8 +172,14 @@ def lockin_DAQ_update():
     state.rb_ref_Y.extend(state.ref_Y)
     state.rb_sig_I.extend(state.sig_I)
 
-    window.hcc_ref_X.extendData(state.time, state.ref_X)
-    window.hcc_ref_Y.extendData(state.time, state.ref_Y)
+    # Note: `ref_X` [non-dim] is transformed to `ref_X*` [V]
+    # Note: `ref_Y` [non-dim] is transformed to `ref_Y*` [V]
+    window.hcc_ref_X.extendData(
+        state.time, np.multiply(state.ref_X, c.ref_V_ampl_RMS) + c.ref_V_offset
+    )
+    window.hcc_ref_Y.extendData(
+        state.time, np.multiply(state.ref_Y, c.ref_V_ampl_RMS) + c.ref_V_offset
+    )
     window.hcc_sig_I.extendData(state.time, state.sig_I)
 
     # Stage 1
@@ -194,24 +200,8 @@ def lockin_DAQ_update():
         old_ref_Y    = state.rb_ref_Y[valid_slice]
 
         # Heterodyne mixing
-        # Equivalent to SLOW code:
-        #   mix_X =
-        #       (old_ref_X - c.ref_V_offset) / c.ref_V_ampl * c.ref_RMS_factor *
-        #       filt_I
-        #   mix_Y =
-        #       (old_ref_Y - c.ref_V_offset) / c.ref_V_ampl * c.ref_RMS_factor *
-        #       filt_I
-        np.subtract(old_ref_X, c.ref_V_offset  , out=old_ref_X)
-        np.subtract(old_ref_Y, c.ref_V_offset  , out=old_ref_Y)
-        np.divide  (old_ref_X, c.ref_V_ampl    , out=old_ref_X)
-        np.divide  (old_ref_Y, c.ref_V_ampl    , out=old_ref_Y)
-        np.multiply(old_ref_X, c.ref_RMS_factor, out=old_ref_X)
-        np.multiply(old_ref_Y, c.ref_RMS_factor, out=old_ref_Y)
-        np.multiply(old_ref_X, state.filt_I    , out=state.mix_X)
-        np.multiply(old_ref_Y, state.filt_I    , out=state.mix_Y)
-
-        # NOTE:
-        # mix_X, mix_Y, X, Y and R are now in units of [V_rms], not [V] anymore!
+        np.multiply(old_ref_X, state.filt_I, out=state.mix_X)
+        np.multiply(old_ref_Y, state.filt_I, out=state.mix_Y)
     else:
         state.time_1.fill(np.nan)
         old_sig_I = np.full(c.BLOCK_SIZE, np.nan)
@@ -262,24 +252,6 @@ def lockin_DAQ_update():
     state.Y_avg = np.mean(state.Y)
     state.R_avg = np.mean(state.R)
     state.T_avg = np.mean(state.T)
-
-    # Additionally calculate the TRUE RMS value of `R`
-    # NOTE: Don't calculate the rms value based on `np.std(state.sig_I)`
-    # because the sample size is too small, namely of length
-    # 'block_size'. This could cause the rescaled `R` value in units of
-    # V_rms to oscillate slightly whenever a non-integer number of
-    # signal periods is contained inside the block size.
-    #
-    # Instead, we base the rms value on the larger sample size as
-    # contained by the ringbuffer of `sig_I`. This sample size is
-    # `N_blocks * block_size` large, effectively averaging out the
-    # effect of any non-integer numbers of signal periods.
-    #
-    # 'block_size` is set in the microcontroller firmware.
-    # `N_blocks` is defined in the constructor of class `Alia_qdev()`.
-    state.R_avg_trms = (
-        state.R_avg / c.ref_RMS_factor * c.ref_V_ampl / np.std(state.rb_sig_I)
-    )
 
     state.rb_time_2.extend(state.time_2)
     state.rb_X.extend(state.X)
@@ -336,11 +308,11 @@ def write_header_to_log():
                 "ref_Y*[V]",
                 "sig_I[V]",
                 "filt_I[V]",
-                "mix_X[V_RMS]",
-                "mix_Y[V_RMS]",
-                "X[V_RMS]",
-                "Y[V_RMS]",
-                "R[V_RMS]",
+                "mix_X[V]",
+                "mix_Y[V]",
+                "X[V]",
+                "Y[V]",
+                "R[V]",
                 "T[deg]",
             )
         )
@@ -352,16 +324,21 @@ def write_header_to_log():
 def write_data_to_log():
     if alia_qdev.firf_2_mix_X.filter_has_settled:
         # All filters have settled --> green light
-        N = alia.config.BLOCK_SIZE
+        c = alia.config
+        N = c.BLOCK_SIZE
         state = alia_qdev.state
         idx_offset = alia_qdev.firf_1_sig_I.rb_valid_slice.start
 
         # tick = Time.perf_counter()
+        # Note: `ref_X` [non-dim] is transformed to `ref_X*` [V]
+        # Note: `ref_Y` [non-dim] is transformed to `ref_Y*` [V]
         data = np.asmatrix(
             [
                 state.rb_time[:N] / 1e6,
-                state.rb_ref_X[:N],
-                state.rb_ref_Y[:N],
+                np.multiply(state.rb_ref_X[:N], c.ref_V_ampl_RMS)
+                + c.ref_V_offset,
+                np.multiply(state.rb_ref_Y[:N], c.ref_V_ampl_RMS)
+                + c.ref_V_offset,
                 state.rb_sig_I[:N],
                 state.rb_filt_I[idx_offset : idx_offset + N],
                 state.rb_mix_X[idx_offset : idx_offset + N],

debug_Alia_protocol_serial.py
@@ -6,7 +6,7 @@ Minimal running example for trouble-shooting library
 __author__ = "Dennis van Gils"
 __authoremail__ = "vangils.dennis@gmail.com"
 __url__ = "https://github.com/Dennis-van-Gils/DvG_Arduino_lock-in_amp"
-__date__ = "27-07-2021"
+__date__ = "26-08-2021"
 __version__ = "2.0.0"
 # pylint: disable=invalid-name
 
@@ -64,7 +64,8 @@ if __name__ == "__main__":
     N_SETS = 3
     N_REPS = 41
 
-    N_deque = alia.config.BLOCK_SIZE * N_REPS
+    c = alia.config
+    N_deque = c.BLOCK_SIZE * N_REPS
     deque_time = deque(maxlen=N_deque)
     deque_ref_X = deque(maxlen=N_deque)
     deque_ref_Y = deque(maxlen=N_deque)
@@ -107,6 +108,11 @@ if __name__ == "__main__":
                 if fVerbose:
                     print("%3d: %d" % (i_rep, N_samples))
 
+                # Note: `ref_X` [non-dim] is transformed to `ref_X*` [V]
+                # Note: `ref_Y` [non-dim] is transformed to `ref_Y*` [V]
+                ref_X = np.multiply(ref_X, c.ref_V_ampl_RMS) + c.ref_V_offset
+                ref_Y = np.multiply(ref_Y, c.ref_V_ampl_RMS) + c.ref_V_offset
+
                 f_log.write("samples received: %i\n" % N_samples)
                 for i in range(N_samples):
                     f_log.write(
@@ -148,7 +154,7 @@ if __name__ == "__main__":
             ax.plot(np_time / 1e3, deque_sig_I, ".-r")
             ax.set(
                 xlabel="time (ms)",
-                ylabel="y",
+                ylabel="voltage (V)",
                 title=(str_info1 + "\n" + str_info2),
             )
             ax.grid()